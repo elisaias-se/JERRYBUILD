149ed26542f2434759be2ccf7106a63846b9bf44
unknown <elisaiasedu@gmail.com>
2026-05-06 03:50:50 -0400

    Updated camera Initalization for Jetson comatibility

--- Vision/main.py
+++ Vision/main.py
@@ -12,7 +12,7 @@ FOCAL_LENGTH = 700
 VALID_CLASSES = ["bottle", "cup"]
 
 print("📷 Opening camera...")
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("ERROR: Camera failed to open")
