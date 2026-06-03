d6d6295e72ea4c71aad5ea5f71d06e79d062b772
unknown <elisaiasedu@gmail.com>
2026-06-03 16:59:59 -0400

    Changed arm_motor_commands to match added PCA9685 driver

--- ardunio/arm_motor_commands.ino
+++ ardunio/arm_motor_commands.ino
@@ -1,12 +1,18 @@
+/* Upload code to connected Arduino Mega 2560
+   Controls servos through PCA9685 servo driver
+*/
+
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define SERVOMIN  150
-#define SERVOMAX  600
+// Servo pulse calibration
+#define SERVOMIN 150
+#define SERVOMAX 600
 #define SERVO_FREQ 50
 
+// PCA9685 channel assignments
 const int BASE_CH = 0;
 const int SHOULDER_CH = 1;
 const int ELBOW_CH = 2;
@@ -19,6 +25,9 @@ int elbowPos = 90;
 int wristPos = 90;
 int gripperPos = 120;
 
+const int GRIPPER_OPEN = 120;
+const int GRIPPER_CLOSED = 55;
+
 void setup() {
   Serial.begin(115200);
 
@@ -27,6 +36,8 @@ void setup() {
   delay(10);
 
   homeArm();
+
+  Serial.println("PCA9685 arm controller ready");
 }
 
 void loop() {
@@ -49,20 +60,18 @@ void loop() {
     else if (command.startsWith("POSE")) {
       handlePoseCommand(command);
     }
+    else {
+      Serial.println("ERROR: Unknown command");
+    }
   }
 }
 
-int angleToPulse(int angle) {
-  angle = constrain(angle, 0, 180);
-  return map(angle, 0, 180, SERVOMIN, SERVOMAX);
-}
-
-void writeServo(int channel, int angle) {
-  pwm.setPWM(channel, 0, angleToPulse(angle));
-}
-
 void handlePoseCommand(String command) {
-  int base, shoulder, elbow, wrist, gripper;
+  int base;
+  int shoulder;
+  int elbow;
+  int wrist;
+  int gripper;
 
   int parsed = sscanf(
     command.c_str(),
@@ -78,15 +87,30 @@ void handlePoseCommand(String command) {
     moveArmToPose(base, shoulder, elbow, wrist, gripper);
     Serial.println("OK");
   } else {
-    Serial.println("ERROR INVALID POSE");
+    Serial.println("ERROR: Invalid POSE command");
   }
 }
 
+int angleToPulse(int angle) {
+  angle = constrain(angle, 0, 180);
+  return map(angle, 0, 180, SERVOMIN, SERVOMAX);
+}
+
+void writeServo(int channel, int angle) {
+  pwm.setPWM(channel, 0, angleToPulse(angle));
+}
+
 void homeArm() {
-  moveArmToPose(90, 90, 90, 90, 120);
+  moveArmToPose(90, 90, 90, 90, GRIPPER_OPEN);
 }
 
 void moveArmToPose(int base, int shoulder, int elbow, int wrist, int gripper) {
+  base = constrain(base, 0, 180);
+  shoulder = constrain(shoulder, 0, 180);
+  elbow = constrain(elbow, 0, 180);
+  wrist = constrain(wrist, 0, 180);
+  gripper = constrain(gripper, 0, 180);
+
   smoothMove(BASE_CH, basePos, base);
   smoothMove(SHOULDER_CH, shoulderPos, shoulder);
   smoothMove(ELBOW_CH, elbowPos, elbow);
@@ -95,11 +119,11 @@ void moveArmToPose(int base, int shoulder, int elbow, int wrist, int gripper) {
 }
 
 void openGripper() {
-  smoothMove(GRIPPER_CH, gripperPos, 120);
+  smoothMove(GRIPPER_CH, gripperPos, GRIPPER_OPEN);
 }
 
 void closeGripper() {
-  smoothMove(GRIPPER_CH, gripperPos, 55);
+  smoothMove(GRIPPER_CH, gripperPos, GRIPPER_CLOSED);
 }
 
 void smoothMove(int channel, int &currentPos, int targetPos) {
